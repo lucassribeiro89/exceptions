7011a052f94915f85797b4d3cb85dd7833886351
Lucas Silvestre <lucassribeiro.adv@gmail.com>
2020-06-23 14:18:38 -0300

    projeto de exceptions

---
+++ src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		try {
+			acc.withdraw(amount);
+			System.out.print("New balance: " + String.format("%.2f", acc.getBalance()));
+		}
+		catch (DomainException e) {
+			System.out.println("Withdraw Error: " + e.getMessage());
+		}
+	
+		sc.close();
+
+	}
+
+}
